a52bb5bfacbeac500bc2f4ead83b34c8cf482f1d
Alejandro Lopez <alejandro.lopez@phila.gov>
2019-11-21 16:04:41 -0500

    feat: adding footer component
    Adding the footer component with the hability to render links with a mandatory href and text properties

--- src/components/AppFooter/AppFooter.vue
+++ src/components/AppFooter/AppFooter.vue
@@ -1,8 +1,34 @@
 <template>
-  <footer class="footer is-primary">
+  <footer class="footer">
     <div class="has-text-centered">
-      <ul>
-        <li><a href="">Feedback</a></li>
+      <ul class="inline-list">
+        <li
+          v-for="(link, index) in localLinks"
+          :key="index"
+        >
+          <template v-if="typeof link.href === 'function'">
+            <a
+              v-bind="link.attrs || {}"
+              @click.prevent="link.href"
+            >
+              {{ link.text }}
+            </a>
+          </template>
+          <template v-else-if="!isURL(link.href)">
+            <router-link
+              :to="link.href"
+              v-bind="link.attrs || {}"
+            >
+              {{ link.text }}
+            </router-link>
+          </template>
+          <template v-else-if="isURL(link.href)">
+            <a
+              :href="link.href"
+              v-bind="link.attrs || {}"
+            >{{ link.text }}</a>
+          </template>
+        </li>
       </ul>
     </div>
   </footer>
@@ -10,6 +36,39 @@
 <script>
 export default {
   name: 'AppFooter',
+  props: {
+    links: {
+      type: Array,
+      default: () => {
+        return []; // Each link requires at least a href and text properties.
+      },
+    },
+  },
+  computed: {
+    localLinks() {
+      // Prepare links
+      let localLinks = [];
+
+      this.links.forEach(link => {
+        let defaults =  { href: "#", text: "link here", "attrs": {}};
+        Object.keys(link).forEach(key =>  {
+          if (defaults[key]) {
+            defaults[key] = link[key];
+          } else {
+            defaults.attrs[key] = link[key];
+          }
+        });
+        localLinks.push(Object.assign({}, defaults));
+      });
+
+      return localLinks;
+    },
+  },
+  methods: {
+    isURL(link) {
+      return (/https?:\/\//.test(link));
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
@@ -20,7 +79,6 @@ export default {
       bottom: 0;
       width: 100%;
       padding: rem-calc(10px);
-
       ul {
         list-style: none;
         padding: 0;
