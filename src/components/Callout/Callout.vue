301b86cfc4e24e9ae957d62dfb86887defa1f344
stevetotheizz0 <stephen.skilton@phila.gov>
2019-12-10 15:52:33 -0500

    feat: Added callout component

---
+++ src/components/Callout/Callout.vue
@@ -0,0 +1,22 @@
+<template>
+  <article class="message">
+    <div :class="'message-body'">
+      <div
+        v-html="message"
+      />
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Callout',
+  props: {
+    message: {
+      type: String,
+      default: 'Your message here.',
+      required: true,
+    },
+  },
+};
+</script>
