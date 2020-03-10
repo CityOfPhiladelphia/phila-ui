568d09e60a20ada4b71d128444568888a8269431
Daniel Faria <daniel.faria@phila.gov>
2020-03-10 11:03:09 -0400

    adding styleguidist

---
+++ styleguide.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // set your styleguidist configuration here
+  title: 'Phila-UI Style Guide',
+  components: 'src/components/**/[A-Z]*.vue',
+  sections: [
+    {
+      name: 'First Section',
+      components: 'src/components/**/[A-Z]*.vue',
+    },
+  ],
+  exampleMode: 'expand',
+};
