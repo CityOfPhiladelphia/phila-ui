e2f71094fb41a7d2be7d5614d7cf6b4167870ed9
Daniel Faria <daniel.faria@phila.gov>
2020-06-05 16:40:54 -0400

    Added new styles to Textbox component

--- src/components/Inputs/Textbox/Textbox.vue
+++ src/components/Inputs/Textbox/Textbox.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="input-wrap input-text"
-    :class="{ required: $attrs.required !== undefined }"
+    :class="{ required: $attrs.required !== undefined, 'has-error': error }"
   >
     <input
       :id="id"
@@ -12,11 +12,17 @@
       :aria-required="$attrs.required !== undefined"
       :value="value"
       :type="type"
-      :placeholder="placeholder"
+      :placeholder="$attrs.required !== undefined ? placeholder + '*' : placeholder"
       @input="$emit('input', $event.target.value)"
       @on="$listeners"
     >
     <label :for="id">{{ placeholder }}</label>
+    <template v-if="error">
+      <div class="input-error-msg">
+        <span class="icon"><i class="fas fa-exclamation-circle" /></span>
+        <span>{{ error }}</span>
+      </div>
+    </template>
   </div>
 </template>
 <script>
@@ -47,37 +53,80 @@ export default {
       type: String,
       default: 'Insert placeholder here',
     },
+    error: {
+      type: String,
+      default: null,
+    },
   },
 };
 </script>
 <style lang="scss">
   @import '../../../styles/inputs.scss';
 
+  .input {
+    font-size: 1.063rem;
+    line-height: 1.5rem;
+    height: 3.5rem;
+    color: $dark-gray;
+    padding: 1.1rem 1rem 1rem 1rem;
+    border-bottom: 1px solid #979797;
+    &:not(:placeholder-shown),
+    &:focus {
+      padding: 1rem 1rem 0 1rem;
+      + label {
+        opacity: 1;
+      }
+    }
+    &:focus {
+      background-color: $white;
+      border: 2px solid $electric-blue !important;
+    }
+    &:hover {
+      border-bottom: 1px solid #979797;
+    }
+  }
+
+  .input:not(:-ms-input-placeholder) {
+    padding: 1rem 1rem 0 1rem;
+    + label {
+      opacity: 1;
+    }
+  }
+
   .input-text {
     position: relative;
-    label {
-      position: absolute;
-      top: 0px;
-      left: 10px;
-      z-index: 5;
-      font-size: 10px;
-      color: $dark-gray;
-      font-weight: 300;
-      opacity: 0;
+    padding: 0.5rem 0.5rem 0.25rem;
+    &.has-error {
+      background-color: $error-light;
+      .input {
+        border: 2px solid $love-park-red;
+      }
+      .input-error-msg {
+        font-size: 0.813rem;
+        color: $love-park-red;
+      }
     }
-    input[type="text"],
-    input[type="email"],
-    input[type="password"],
-    input[type="search"],
-    input[type="tell"],
-    input[type="number"] {
-      &:focus {
-        border-width: 2px;
-        padding-bottom: 0;
-        + label {
-          opacity: 1;
+    &.required {
+      label {
+        &:after {
+          content: "*";
+          font-size: 0.813rem;
         }
       }
     }
   }
+
+  label {
+    position: absolute;
+    top: 0.5rem;
+    left: 0.5rem;
+    z-index: 5;
+    font-size: 0.813rem;
+    color: #666666;
+    font-weight: 400;
+    line-height: 1.5rem;
+    padding: 0 0 0 0.90rem;
+    opacity: 0;
+  }
+
 </style>
