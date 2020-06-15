5d86a57fdafb972a6fe056751a4b07e689589eb4
Daniel Faria <daniel.faria@phila.gov>
2020-06-15 09:37:26 -0400

    Added error styles

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
+const path = require('path');
+
 module.exports = {
   lintOnSave: true,
   css: {
     loaderOptions: {
       sass: {
         data: `
-          @import "@/styles/all.scss";
+          @import "@/styles/styleguide-all.scss";
         `,
       },
     },
