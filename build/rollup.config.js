897a6c74d2b6551cafdd96678d56f4b7ab763f5d
Daniel Faria <daniel.faria@phila.gov>
2021-07-16 15:42:36 -0400

    A series of packages updates, linting, and bug fixes: - updates babelrc config - updates package.json config - updates rollup config - bumps rollup to latest, along with newer version of rollup plugins - npm audit fixes - npm linting fixes - bumps bulma to latest which fixes a deprecated warning - fixes browserlistsrc

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,35 +1,69 @@
-import commonjs from 'rollup-plugin-commonjs'; // Convert CommonJS modules to ES6
-import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
+
+//Convert CommonJS modules to ES6
+import commonjs from '@rollup/plugin-commonjs';
+
+//Locate and bundle third-party dependencies in node_modules
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+//Compile your files with Babel
 import babel from '@rollup/plugin-babel';
+
+//Import JPG, PNG, GIF, SVG, and WebP files
 import image from '@rollup/plugin-image';
+
+//Define and resolve aliases for bundle dependencies
 import alias from '@rollup/plugin-alias';
+
+//Handle .vue SFC files
+import vue from 'rollup-plugin-vue';
+
 import path from 'path';
 
 const projectRootDir = path.join(__dirname, '..');
 
 export default [ 'phila-ui' ].map((name) => ({
   input: `src/${name}.js`,
-  output: [{
-    format: 'umd',
-    name,
-    file: `dist/${name}.umd.js`,
-  },
-  {
-    format: 'es',
-    name,
-    exports: 'named',
-    file: `dist/${name}.esm.js`,
-  }, {
-    format: 'iife',
-    name,
-    extend: true,
-    exports: 'named',
-    file: `dist/${name}.min.js`,
-  },
+  output: [
+    {
+      format: 'umd',
+      name,
+      file: `dist/${name}.umd.js`,
+      globals: {
+        vue: 'Vue',
+      },
+    },
+    {
+      format: 'es',
+      name,
+      exports: 'named',
+      file: `dist/${name}.esm.js`,
+      globals: {
+        vue: 'Vue',
+      },
+    },
+    {
+      format: 'iife',
+      name,
+      extend: true,
+      exports: 'named',
+      file: `dist/${name}.min.js`,
+      globals: {
+        vue: 'Vue',
+      },
+    },
+  ],
+  external: [
+    /@babel\/runtime/,
+    'vue',
   ],
   plugins: [
     image(),
+    nodeResolve(),
     commonjs(),
+    babel({
+      exclude: 'node_modules/**',
+      babelHelpers: 'runtime',
+    }),
     vue({
       css: true, // Dynamically inject css as a <style> tag
       compileTemplate: true, // Explicitly convert template to render function,
@@ -46,10 +80,6 @@ export default [ 'phila-ui' ].map((name) => ({
         },
       },
       transformAssetUrls: true,
-    }),
-    babel({
-      exclude: 'node_modules/**',
-      babelHelpers: 'runtime',
     }),
     alias({
       entries: [
